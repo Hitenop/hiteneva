f0768b62a3ac8e0472dcd154f12a329b21a72280
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-12-18 09:52:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ class script(object):
 - Updates - https://t.me/CTP_Official  
 
 <b>DEVS:</b>
-- <a href=https://t.me/CTP_OFFICIAL>Team Eva Maria</a>"""
+- <a href=https://t.me/CTP_OFFICIAL>CTP OFFICIAL</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
